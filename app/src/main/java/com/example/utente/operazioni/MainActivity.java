fe108bcd0901598c4f76847e6de1b61cea2573b4
Aequinoxio <prikeprok@gmail.com>
2017-02-17 21:06:20 +0100

    Inserite alcune animazioni ed il menu about

--- app/src/main/java/com/example/utente/operazioni/MainActivity.java
+++ app/src/main/java/com/example/utente/operazioni/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.utente.operazioni;
 import android.animation.Animator;
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
+import android.content.DialogInterface;
 import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -32,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout lay;
     int colorBackground;
 
+    CustomKeyboard mCustomKeyboard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        mCustomKeyboard= new CustomKeyboard(this, R.id.keyboardview, R.xml.hexkbd );
+        mCustomKeyboard.registerEditText(R.id.edRisposta);
 
         lay=(LinearLayout)findViewById(R.id.layMain);
         colorBackground=lay.getSolidColor();
@@ -44,17 +54,54 @@ public class MainActivity extends AppCompatActivity {
         EditText ed = (EditText) findViewById(R.id.edRisposta);
         ed.requestFocus();
 
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-//        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-//        imm.showSoftInput(ed, InputMethodManager.SHOW_IMPLICIT);
         prossimaOperazione();
     }
+
+    @Override public void onBackPressed() {
+        // NOTE Trap the back key: when the CustomKeyboard is still visible hide it, only when it is invisible, finish activity
+        if( mCustomKeyboard.isCustomKeyboardVisible() ) mCustomKeyboard.hideCustomKeyboard(); else this.finish();
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // About
+        if (id == R.id.action_about) {
+            String s = getString(R.string.app_name) +" - Ver. " + BuildConfig.VERSION_NAME ;
+            s+="\nby "+ getString(R.string.Autore);
+            s+="\n\n"+getString(R.string.descrizione);
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.action_about)
+                    .setMessage(s)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    }).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     private void prossimaOperazione(){
         Random n=new Random(System.currentTimeMillis());
@@ -71,12 +118,17 @@ public class MainActivity extends AppCompatActivity {
             fattore2=n.nextInt(11);
             risultato = fattore1 + fattore2;
         }
-        TextView txtView= (TextView) findViewById(R.id.txtOperazione);
 
+        // Animazione fade out del vecchio
+        TextView txtView= (TextView) findViewById(R.id.txtOperazione);
+        txtView.startAnimation(AnimationUtils.loadAnimation(this,android.R.anim.fade_out));
         if (moltiplOAddiz)
             txtView.setText(String.format("%d x %d", fattore1,fattore2));
         else
             txtView.setText(String.format("%d + %d", fattore1,fattore2));
+
+        // Animazione fade in del nuovo
+        txtView.startAnimation(AnimationUtils.loadAnimation(this,android.R.anim.fade_in));
     }
 
     public void checkResult(View v){
@@ -90,23 +142,38 @@ public class MainActivity extends AppCompatActivity {
 
         ed.setText("");
 
+        TextView txtRes;
+        txtRes=(TextView)findViewById(R.id.txtOperazione);
+
         if (guessed==risultato){
+            txtRes.startAnimation(AnimationUtils.loadAnimation(this,R.anim.zoom_in));
             animateBackgroudColor(Color.GREEN,colorBackground,colorBackground);
             showToastResult("Ben fatto!!!\nProva con un'altra operazione");
             ultimaRisposta=true;
             risultatoOK++;
         } else {
+            // Animazione dell'operazione (zoom)
+            txtRes.startAnimation(AnimationUtils.loadAnimation(this,R.anim.zoom_out));
             animateBackgroudColor(Color.RED,colorBackground,colorBackground);
             showToastResult("Sbagliato\nRiprova");
             ultimaRisposta=false;
             risultatoKO++;
         }
 
+
         // Aggiorno le label di riepilogo. Può essere ottimizzato inserendo nei punti corretti delle IF le parti relative al risultato
-        TextView txtRes=(TextView)findViewById(R.id.txtResultOk);
-        txtRes.setText(String.valueOf(risultatoOK));
-        txtRes=(TextView)findViewById(R.id.txtResultKO);
-        txtRes.setText(String.valueOf(risultatoKO));
+        if (ultimaRisposta){
+            txtRes=(TextView)findViewById(R.id.txtResultOk);
+
+            txtRes.startAnimation(AnimationUtils.loadAnimation(this,android.R.anim.slide_in_left));
+            txtRes.setText(String.valueOf(risultatoOK));
+
+        } else {
+            txtRes = (TextView) findViewById(R.id.txtResultKO);
+
+            txtRes.startAnimation(AnimationUtils.loadAnimation(this,R.anim.slide_in_right));
+            txtRes.setText(String.valueOf(risultatoKO));
+        }
     }
 
     // Preso dalla documentazione ufficiale di google https://developer.android.com/guide/topics/ui/controls/radiobutton.html
